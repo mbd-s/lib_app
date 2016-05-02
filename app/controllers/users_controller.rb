a1d0ed58c8e8f32dbcc061bd28f47e355c1292d9
mbd-s <michaelbsilverberg@gmail.com>
2016-05-02 16:48:20 -0700

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
     userLookingAtOwnPage = @user.id == current_user.id
-    if userLookingAtOwnPage
+    if !!userLookingAtOwnPage
       render :show
     else
       redirect_to login_path
