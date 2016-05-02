ac3f1869f58fc2878041d5949b2173c57e918ebe
mbd-s <michaelbsilverberg@gmail.com>
2016-05-02 14:37:19 -0700

    set up routes and models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
     render :index
   end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
+
 end
