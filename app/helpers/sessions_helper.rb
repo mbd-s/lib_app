6f8a69b58112efc76ace9ce47efe0b696ba64648
mbd-s <michaelbsilverberg@gmail.com>
2016-05-03 16:43:43 -0700

    added an error message

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,7 @@ module SessionsHelper
   def logged_in?
     if current_user == nil
       redirect_to new_session_path
+      flash[:error] = "You must be logged in to see that."
     end
   end
 
